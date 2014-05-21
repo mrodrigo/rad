a970664cf08b785cade38383f05b103f673797b8
ivives <ivives75@gmail.com>
2014-05-21 10:35:30 +0200

    TreeViewFiller

--- PGTKarticulo/PGTKarticulo/TreeViewFiller.cs
+++ PGTKarticulo/PGTKarticulo/TreeViewFiller.cs
@@ -9,6 +9,40 @@ namespace PGTKarticulo
 	{
 		static void Fill(TreeView treeView, IDbconnection dbConnection, string selectText)
 		{
+			IDbCommand.CommandText= "select * from articulo";
+
+			IDataReader dataReader= IDbCommand.ExecuteReader();
+
+ 			for (int index=0; index<mySqlDataReader.FieldCount;index ++)
+			treeView.AppendColumn(mySqlDataReader.GetName(index),new CellRendererText(),"text",index);
+
+			int fieldCount = mySqlDataReader.FieldCount;
+			
+			ListStore listStore= createListStore(fieldCount);
+			
+			 while(mySqlDataReader.Read()){
+			
+				string []line= new string[mySqlDataReader.FieldCount];
+				for (int index=0; index< mySqlDataReader.FieldCount;index++){
+					object value= mySqlDataReader.GetValue(index);
+					line[index]=value.ToString();
+				 }
+			
+				listStore.AppendValues(line);
+				
+				treeView.Model=listStore;
+				}
+				dataReader.Close();
+				IDbCommand.CommandText= "select * from articulo";
+				
+			}
+			static ListStore createListStore(int fieldCount){
+				Type[] types = new Type[fieldCount];
+				for (int index = 0; index < fieldCount; index++)
+				types[index] = typeof(string);
+				return new ListStore(types);
+			} 
+			
 		}
 		
 		
